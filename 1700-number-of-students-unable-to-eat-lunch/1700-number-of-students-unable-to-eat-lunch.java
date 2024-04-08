2b95a738f8bd63b0e17abd4119ec9bcea615e1bc
Deepa bisht <98414848+giluu8@users.noreply.github.com>
2024-04-08 22:14:20 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (61.63%) - LeetHub

---
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int countStudents(int[] students, int[] sandwiches) {
+        int circle=0;
+        int square=0;
+        for(int choice:students){
+            if (choice==0){
+                circle++;
+            }
+            else{
+                square++;
+            }
+        }
+            for(int sandwich:sandwiches){
+                if(sandwich==0 && circle==0){
+                    return square;
+                }
+                if(sandwich==1 &&square==0){
+                    return circle;
+                }
+                if(sandwich==0){
+                    circle--;
+                }
+                else{
+                    square--;
+                }
+            }
+            
+                return 0;
+            }
+        }
+    
+    
+
