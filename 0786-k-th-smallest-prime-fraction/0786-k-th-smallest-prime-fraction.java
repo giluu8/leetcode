e7ca4570d5247b905ee6b01ff05740dc60c8b45c
Deepa bisht <98414848+giluu8@users.noreply.github.com>
2024-05-10 22:56:22 +0530

    Time: 363 ms (57.20%), Space: 71.5 MB (39.30%) - LeetHub

---
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] kthSmallestPrimeFraction(int[] arr, int k) {
+        PriorityQueue<double[]> pq = new PriorityQueue<>((a, b) -> Double.compare(b[0], a[0]));
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                double div = (double) arr[i] / arr[j];
+                pq.offer(new double[]{div, (double) arr[i], (double) arr[j]});
+                if (pq.size() > k)
+                    pq.poll();
+            }
+        }
+
+        double[] vec = pq.peek();
+        int[] result = new int[2];
+        result[0] = (int) vec[1];
+        result[1] = (int) vec[2];
+        return result;
+    }
+}
+    
